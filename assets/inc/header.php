8248ba1f1a03402a5e14947facecb9822a4de6c1
dragon1211 <inddev0807@gmail.com>
2022-01-07 16:14:50 -0800

    add project

---
+++ assets/inc/header.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>順天堂医院</title>
+  <meta name="description" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:title" content="">
+  <meta property="og:description" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="">
+  <meta name="twitter:image" content="">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    <header class="header js-header wow fadeIn">
+      <h1 class="header-logo">
+        <a href="/">
+          <img class="js-logo-pc h-logo-pc" src="./assets/images/common/h-logo.svg" alt="">
+          <img class="js-logo-sp h-logo-sp" src="./assets/images/common/h-logo-sm-white.svg" alt="">
+        </a>
+      </h1>
+      <div class="sp-only sp-navBtn js-sp-navBtn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="menu js-menu">
+        <div class="menu-inner">
+          <ul class="menu-list">
+            <li class="menu-item">
+              <a href="/"><span>患者さん・一般の方へ</span></a>
+            </li>
+            <li class="menu-item">
+              <a href="/"><span>研究者の方へ</span></a>
+            </li>
+            <li class="menu-item">
+              <a href="/"><span>企業の方へ</span></a>
+            </li>
+            <li class="menu-item">
+              <a href="/"><span>当センターのご案内</span></a>
+            </li>
+            <li class="menu-item trans-en">
+              <a href="/"><span>ENGLISH</span></a>
+            </li>
+            <li class="menu-item se-img">
+              <a href="/">
+                <img class="pc-only" src="./assets/images/common/search.svg" alt="">
+                <img class="sp-only" src="./assets/images/common/search-white.svg" alt="">
+              </a>
+            </li>
+          </ul>
+          <p class="menu-item contact pc-only">
+            <a href="/contact">
+              お問い合わせ
+            </a>
+          </p>
+        </div>
+      </nav>
+    </header>
+
